cebf208f4b40315c7ccb7e727a8e99a61dd9f6b4
Mk-003 <152386180+Mk-003@users.noreply.github.com>
2024-02-16 10:57:27 +0300

    Update Footer.js

---
+++ movie/src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Movies App</p>
+      <p>Created by: 
+        Mayar,
+        Steve,
+        Victor,
+        Brian,
+        Joseph.
+        </p>
+    </footer>
+  );
+}
+
+export default Footer;
